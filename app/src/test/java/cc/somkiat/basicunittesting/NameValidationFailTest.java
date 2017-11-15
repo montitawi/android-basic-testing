adf8ed4f4a99dbb02540a70379389e67abb2834e
montitawi <58070114@kmtil.ac.th>
2017-11-16 01:56:48 +0700

    Use ValidationResult at Test Case

--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
@@ -9,22 +9,22 @@ public class NameValidationFailTest {
     @Test
     public void nameIsNull() {
         NameValidation validation = new NameValidation();
-        boolean result = validation.isEmpty("");
-        assertFalse("Name is Empty !", result);
+        ValidationResult result = validation.validateName("");
+        assertFalse(result.getMessage(), result.isValidationValid());
     }
 
     @Test
     public void nameLengthNotInRangeTwoToTwenty() {
         NameValidation validation = new NameValidation();
-        boolean result = validation.isNameLengthBetweenTwoToTwenty("m");
-        assertFalse("Name is short !", result);
+        ValidationResult result = validation.validateName("m");
+        assertFalse(result.getMessage(), result.isValidationValid());
     }
 
     @Test
     public void nameIsInCorrectPattern() {
         NameValidation validation = new NameValidation();
-        boolean result = validation.isNameCollect("Mon ti ta");
-        assertFalse("Name is incorrect Pattern !", result);
+        ValidationResult result = validation.validateName("Mon ti ta");
+        assertFalse(result.getMessage(), result.isValidationValid());
     }
 
 }
