79cfa5effa63d0e927a45c8a52cf06870d1004fe
montitawi <58070114@kmtil.ac.th>
2017-11-16 01:55:22 +0700

    Create validateEmail and Add throw at Test Case

--- app/src/main/java/cc/somkiat/basicunittesting/EmailValidation.java
+++ app/src/main/java/cc/somkiat/basicunittesting/EmailValidation.java
@@ -5,12 +5,35 @@ import java.util.regex.Pattern;
 
 class EmailValidation {
 
-    boolean isEmailEmpty(String email){
-        return !email.isEmpty();
-    }
-    boolean isEmailCorrect(String email){
-        String emailPattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
-        return Pattern.matches(emailPattern,email);
+    ValidationResult validateEmail(String email) {
+
+        try {
+            isEmailEmpty(email);
+            isEmailPatternCorrect(email);
+        } catch (ValidationException e) {
+            return new ValidationResult(false, e.getMessage());
+        }
+
+        return new ValidationResult(true, "Validate Success");
 
     }
+
+
+    private void isEmailEmpty(String email) throws ValidationException {
+        if (email.isEmpty()) {
+            throw new ValidationException("Email is Empty !");
+        }
+
+    }
+
+    private void isEmailPatternCorrect(String email) throws ValidationException {
+        String emailPattern = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+
+        if (!Pattern.matches(emailPattern, email)) {
+            throw new ValidationException("Email is incorrect !");
+        }
+
+    }
+
+
 }
