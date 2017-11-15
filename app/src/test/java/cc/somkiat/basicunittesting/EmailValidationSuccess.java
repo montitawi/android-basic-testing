91e6f7e8b7f06d8b50a010cf51bcf0967faceb93
montitawi <58070114@kmtil.ac.th>
2017-11-15 23:56:36 +0700

    Add Test Case emailIsInCorrectPattern

--- app/src/test/java/cc/somkiat/basicunittesting/EmailValidationSuccess.java
+++ app/src/test/java/cc/somkiat/basicunittesting/EmailValidationSuccess.java
@@ -12,5 +12,11 @@ public class EmailValidationSuccess {
         boolean result = validation.isEmailEmpty("montita@mail.com");
         assertTrue("Email is NotEmpty", result);
     }
+    @Test
+    public void emailIsInCorrectPattern(){
+        EmailValidation validation = new EmailValidation();
+        boolean result = validation.isEmailCorrect("montita@mail.com");
+        assertTrue("Email is correct",result);
+    }
 
 }
