8c7c95ec2e6eccc5fbe5c217f73e7a41fe38f90d
montitawi <58070114@kmtil.ac.th>
2017-11-16 01:56:40 +0700

    Use ValidationResult at Test Case

--- app/src/test/java/cc/somkiat/basicunittesting/EmailValidationSuccess.java
+++ app/src/test/java/cc/somkiat/basicunittesting/EmailValidationSuccess.java
@@ -9,14 +9,14 @@ public class EmailValidationSuccess {
     @Test
     public void emailNotNull() {
         EmailValidation validation = new EmailValidation();
-        boolean result = validation.isEmailEmpty("montita@mail.com");
-        assertTrue("Email is NotEmpty", result);
+        ValidationResult result = validation.validateEmail("montita@mail.com");
+        assertTrue(result.getMessage(), result.isValidationValid());
     }
     @Test
     public void emailIsInCorrectPattern(){
         EmailValidation validation = new EmailValidation();
-        boolean result = validation.isEmailCorrect("montita@mail.com");
-        assertTrue("Email is correct",result);
+        ValidationResult result = validation.validateEmail("montita@mail.com");
+        assertTrue(result.getMessage(),result.isValidationValid());
     }
 
 }
